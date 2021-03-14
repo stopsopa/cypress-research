9fb21e9205e76ab12b88941c335f2615751dbccc
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2021-03-14 23:00:46 +0000

    update

--- cypress/integration/examples/first-test.spec.js
+++ cypress/integration/examples/first-test.spec.js
@@ -1,5 +1,8 @@
 /// <reference types="cypress" />
 
+// const url = 'http://localhost:8080'
+const url = 'https://stopsopa.github.io/cypress-research'
+
 context('First tests', () => {
 
   // beforeEach(() => {
@@ -8,6 +11,16 @@ context('First tests', () => {
 
   it('Hello Cypress', () => {
 
+    cy.visit(`${url}/index.html`)
 
+    cy.contains('Hello Cypress')
+
+    cy.get('input')
+      .type('getparam')
+      .should('have.value', 'getparam')
+
+    cy.get('a').click();
+
+    cy.url().should('include', '?url=getparam');
   })
 })
