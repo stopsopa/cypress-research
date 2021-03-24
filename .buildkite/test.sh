bfed896fd9499bb85636f85048f65441be5006c0
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2021-03-24 15:31:42 +0000

    home

--- .buildkite/test.sh
+++ .buildkite/test.sh
@@ -1,4 +1,26 @@
 
+echo "------"
+echo "HOME=$HOME"
+echo "------"
+
+export HOME="/home/buildkite-agent"
+
+source ~/.bashrc
+
+echo "------"
+echo "HOME=$HOME"
+echo "------"
+if [ "$HOME" != "/home/buildkite-agent" ]; then
+
+  echo "HOME is not equal /home/buildkite-agent";
+
+  exit 1
+fi
+
+echo "ROOT=$ROOT"
+
+cd "$ROOT";
+
 set -x          # Expand and print each command before executing
 set -e          # Exit script immediately if any command returns a non-zero exit status.
 set -o pipefail # more: https://buildkite.com/docs/pipelines/writing-build-scripts#configuring-bash
